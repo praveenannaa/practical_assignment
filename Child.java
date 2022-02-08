69fad94185a3c0b156b5721f8a3f32c0b467e073
praveenannaa <98143333+praveenannaa@users.noreply.github.com>
2022-02-08 15:22:10 +0530

    Add files via upload

---
+++ Child.java
@@ -0,0 +1,15 @@
+package com.learn.accessmodianother;
+import com.learn.access.AccessModifiers;
+
+
+public class Child extends AccessModifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child ch=new Child();
+		ch.publicmethod();
+		ch.protectedmethod();
+
+	}
+
+}
